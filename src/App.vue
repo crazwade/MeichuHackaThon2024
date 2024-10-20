8d253b8e6c91d5353d6f13b25be1ceaacfd79f7c
Jacky <jackywu526@gmail.com>
2024-10-20 16:55:53 +0800

    feat(App.vue): import version

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import Layout from './layout/Layout.vue'
 import { useImagesStore } from './stores/images';
+import { version } from '../package.json';
 
 const imagesStore = useImagesStore()
 
 onBeforeMount(() => {
   imagesStore.preLoadImages();
+})
+
+onMounted(() => {
+  console.log('version', version);
 })
 </script>
 
